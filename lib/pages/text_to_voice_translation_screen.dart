73d82fe9222004576a5526c198d83637bc5fcbcd
Pasindu Muthumala <lgpmuthumala@gmail.com>
2023-09-24 23:09:01 +0530

    Make text to voice screen more user friendly.

--- lib/pages/text_to_voice_translation_screen.dart
+++ lib/pages/text_to_voice_translation_screen.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../models/text_to_voice_model.dart';
-
 import '../servises/text_to_voice_service.dart';
+
 
 class TextToVoiceScreen extends StatefulWidget {
   const TextToVoiceScreen({Key? key}) : super(key: key);
@@ -27,36 +27,46 @@ class _TextToVoiceScreenState extends State<TextToVoiceScreen> {
     final textToVoiceModel = Provider.of<TextToVoiceModel>(context);
     final textToSpeechService = TextToVoiceService();
 
-    // to directly speak, use this syntax below
-    // textToSpeechService.speak('hi');
+    final elevatedButtonStyle = ElevatedButton.styleFrom(
+      foregroundColor: Colors.white,
+      backgroundColor: const Color.fromARGB(255, 4, 21, 51),
+      padding: const EdgeInsets.all(10),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+    );
 
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 4, 21, 51),
         title: const Text('Text to Voice Conversion'),
       ),
       body: Container(
         padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+        color: const Color.fromARGB(255, 62, 56, 117),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           mainAxisSize: MainAxisSize.max,
           children: [
             TextField(
+              cursorColor: Colors.white,
               maxLines: 5,
-              controller: textEditingController, // Set the controller here
-              onChanged: (text) {
-                // No need to use setState to update inputText
-                // inputText = text;
-              },
+              controller: textEditingController,
+              style: TextStyle(color: Colors.white), // Set the text color here
+              onChanged: (text) {},
               decoration: const InputDecoration(
                 border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20))),
+                  borderRadius: BorderRadius.all(
+                    Radius.circular(20),
+                  ),
+                ),
                 labelText: 'Enter text to convert',
+                labelStyle: TextStyle(color: Colors.white), // Set label text color
               ),
             ),
             const SizedBox(height: 20),
             Row(
               children: [
                 ElevatedButton(
+                  style: elevatedButtonStyle,
                   onPressed: () {
                     setState(() {
                       textEditingController.clear(); // Clear the text field using the controller
@@ -66,6 +76,7 @@ class _TextToVoiceScreenState extends State<TextToVoiceScreen> {
                 ),
                 const Spacer(),
                 ElevatedButton(
+                  style: elevatedButtonStyle,
                   onPressed: () {
                     textToVoiceModel.setTextToSpeak(textEditingController.text); // Set text using the controller
                     final textToSpeak = textToVoiceModel.textToSpeak;
