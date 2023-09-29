cac8046d1e72c75e1761fd97649bff7fb02b4563
KMjanith <kavinduj.20@cse.mrt.ac.lk>
2023-09-30 02:53:40 +0530

    subscribe status

--- lib/servises/AuthManager.dart
+++ lib/servises/AuthManager.dart
@@ -1,6 +1,7 @@
 class AuthManager {
-  static bool isLoggedIn = false;
+  static bool isLoggedIn = true; //original is flase
   static String token = '';
+  static bool subscribe = true; //original is false
 
   static void login() {
     // Perform login logic
@@ -10,5 +11,13 @@ class AuthManager {
   static void logout() {
     // Perform logout logic
     isLoggedIn = false;
+  }
+
+  static void subscribed() {
+    subscribe = true;
+  }
+
+  static void unSubscribed() {
+    subscribe = false;
   }
 }
